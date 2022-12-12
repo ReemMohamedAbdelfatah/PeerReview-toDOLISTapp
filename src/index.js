29dc3b9ebeb214f10a665f5dc396cfd4619e5283
ReemMohamedAbdelfatah <reem.mohamed.abdelfatah@gmail.com>
2022-12-12 14:28:11 +0200

    Adding all files

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import './style.css';
+import DomToDo from './modules/domDisplay.js';
+import CreateToDo from './modules/todoConstruct.js';
+import Storage from './modules/localStorage.js';
+
+document.addEventListener('DOMContentLoaded', DomToDo.displayToDo);
+document.getElementById('completedBtn').addEventListener('click', () => {
+  Storage.removeCompleted();
+});
+
+document.querySelector('form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const todoL = Storage.getToDo();
+  const toDoInput = document.getElementById('todo-input').value;
+  const id = todoL.length + 1;
+  const completed = false;
+  const todo = new CreateToDo(toDoInput, id, completed);
+  DomToDo.addToDoList(todo);
+  Storage.addTodo(todo);
+  DomToDo.clearField();
+});
+document.getElementById('to-do-container').addEventListener('click', (e) => {
+  Storage.editInput(
+    e.target.parentElement.parentElement.children[4].textContent,
+    e.target.parentElement,
+    e.target.parentElement.parentElement,
+    e.target.parentElement.parentElement.children[2].children[0],
+  );
+  DomToDo.deleteTodo(e.target);
+  if (e.target.classList.contains('check')) {
+    Storage.checkboxCompleted(
+      e.target.parentElement.parentElement.children[4],
+      e.target.checked,
+    );
+    e.target.parentElement.parentElement.children[2].children[0].classList.toggle(
+      'strike-through',
+    );
+  }
+  Storage.remove(
+    e.target.parentElement.previousElementSibling.previousElementSibling
+      .textContent,
+  );
+});
